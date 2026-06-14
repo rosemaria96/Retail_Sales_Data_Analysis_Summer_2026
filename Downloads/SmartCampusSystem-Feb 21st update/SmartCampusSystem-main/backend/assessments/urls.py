e9b2fee07a6d466aec643c1460b2ae7137cecb83
Rose Maria Jose <ranirosejose96@gmail.com>
2026-06-14 20:45:06 +0530

    Delete Downloads/SmartCampusSystem-Feb 21st update/SmartCampusSystem-main directory

--- Downloads/SmartCampusSystem-Feb 21st update/SmartCampusSystem-main/backend/assessments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/question-bank/', views.admin_qn_bank, name='admin_qn_bank'),
-    path('student/question-bank/', views.student_question_bank, name='student_question_bank'),
-    path('teacher/question-bank/', views.teacher_question_bank, name='teacher_question_bank'),
-]
